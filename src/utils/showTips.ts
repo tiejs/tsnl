b91f86c349a68e8086789424a5e719f75e4b149f
forsigner <forsigner@gmail.com>
2019-09-07 09:41:16 +0800

    refactor: improve tips

--- src/utils/showTips.ts
+++ src/utils/showTips.ts
@@ -1,19 +1,18 @@
 import chalk from 'chalk'
-const { green } = chalk
-
+const { green, cyan } = chalk
 
 export function showTips(root: string, appName: string) {
   console.log()
   console.log(`Success! Created ${green(appName)} at ${green(root)}`)
   console.log('Inside that directory, you can run several commands:')
   console.log()
-  console.log(chalk.cyan(`  npm run dev`))
+  console.log(cyan(`  npm run dev`))
   console.log('    Starts the development mode.')
   console.log()
-  console.log(chalk.cyan(`  npm run build`))
+  console.log(cyan(`  npm run build`))
   console.log('    Build for production.')
   console.log()
-  console.log(chalk.cyan(`  npm test`))
+  console.log(cyan(`  npm test`))
   console.log('    Starts the test runner.')
   console.log()
   console.log('Happy hacking!')
